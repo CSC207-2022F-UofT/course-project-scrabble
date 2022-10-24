f9f54b841c4cb9de3e18575532203ee620d5a726
Jazli14 <95947726+Jazli14@users.noreply.github.com>
2022-10-24 15:09:02 -0400

    Add files via upload

---
+++ src/main/java/tutorial/Player.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group.Scrabble.entities;
+
+/**
+ *
+ * @author Clark
+ */
+public class Player {
+    private int score;
+    private String name;
+    private Cell[] hand;
+    
+    public Player(String name) {
+        this.score = 0;
+        this.name = name;
+        this.hand = new Cell[7];
+    }
+    
+    public Player(String loaded_name, int loaded_score, Cell[] loaded_hand) {
+        this.name = loaded_name;
+        this.score = loaded_score;
+        this.hand = loaded_hand;
+    }
+    
+    
+    
+    public void setName(String new_name) {
+        this.name = new_name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setScore(int new_score) {
+        this.score = score;
+    }
+    
+    public int getScore() {
+        return this.score;
+    }
+    
+    public void setHand(Cell[] new_hand) {
+        this.hand = new_hand;
+        
+    }
+    
+    public Cell[] getHand() {
+        return this.hand;
+    }
+}
