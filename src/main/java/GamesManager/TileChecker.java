88d665215c61ad79a355db1ec90a317d6be58fe2
ClaireRamsumair <claire.ramsumair@mail.utoronto.ca>
2022-11-03 13:37:53 -0400

    changed dictionary variable to static, created PlacementChecker interface and TileChecker to determine whether a tile placement is valid

--- src/main/java/GamesManager/TileChecker.java
+++ src/main/java/GamesManager/TileChecker.java
@@ -1,20 +1,16 @@
 package GamesManager;
 
 import entities.GameBoard;
-
 import java.util.Objects;
 
 public class TileChecker implements PlacementChecker {
 
-    private boolean collisionChecker(int row, int column, GameBoard board) {
-        return Objects.equals(board.getBoardCellValue(row, column), "-");
-    }
-
-    private boolean inBoundary(int row, int column, GameBoard board) { // need board size to make this better
-        return row <= 15 & column <= 15; //based on the default board size, 15 can be replaced with board.size or something
-    }
     @Override
     public boolean isValid(int row, int column, GameBoard board) {
-        return collisionChecker(row, column, board) & inBoundary(row, column, board);
+        if (!Objects.equals(board.getBoardCellValue(row, column), "-")) { // checks that no letters are already in that space
+            return false;
+        } else { // need board size to make this better
+            return row <= 15 & column <= 15; //based on the default board size, 15 can be replaced with board.size or something
+        }
     }
 }
