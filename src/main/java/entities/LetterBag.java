4dd38befb7a7e7941daafc9b908587307d6901ac
Victor Zheng <zhengvictor0@gmail.com>
2022-11-07 14:09:23 -0500

    added comment for letter bag for letters

--- src/main/java/entities/LetterBag.java
+++ src/main/java/entities/LetterBag.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
  */
 public class LetterBag {
     private final int[] NUM_LETTERS = {9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 2, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 2};
+    // no blanks, extra z and k
     private final String[] LETTERS = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
     
     private HashMap<String, Integer>bag = new HashMap<>();
