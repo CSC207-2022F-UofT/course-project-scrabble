cbc926399a562037a285e3bdac10ce31c31ddf73
Victor Zheng <zhengvictor0@gmail.com>
2022-11-11 02:54:42 -0500

    basic functionality for swing - front page basically done

--- src/main/java/gui/StartGame.java
+++ src/main/java/gui/StartGame.java
@@ -5,22 +5,27 @@ import java.awt.*;
 
 public class StartGame {
     JFrame f;
-    private final int WIDTH = 1000;
-    private final int HEIGHT = 600;
+    final int WIDTH = 1000;
+    final int HEIGHT = 600;
     public StartGame(){
         f = new JFrame("Scrabble Game");
         JLabel label = new JLabel("Welcome to Scrabble");
         label.setBounds(WIDTH / 3,0,500,200);
         label.setFont(new Font("serif", Font.PLAIN, 40));
-        JLabel contributers = new JLabel("Created by: Victor, Jazli, Umair, Claire, Francisco, Davit, and Clark", SwingConstants.CENTER);
-//        JLabel contributers = new JLabel("Created by: Victor, Jazli, Umair, Claire, Francisco, Davit, and Clark");
-        contributers.setBounds(WIDTH/4, HEIGHT/2, 500,30);
-        contributers.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel contributors = new JLabel("Created by: Victor, Jazli, Umair, Claire, Francisco, Davit, and Clark", SwingConstants.CENTER);
+        contributors.setBounds(WIDTH/4, HEIGHT/4, 500,30);
+        contributors.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        f.add(contributers); f.add(label);
+        StartGameButton sgb = new StartGameButton();
+        sgb.gameButton.setBounds(WIDTH/2, HEIGHT/2, 100,100);
+        f.add(sgb.gameButton);
+        f.add(sgb.tf);
+
+        f.add(contributors); f.add(label);
         f.setSize(WIDTH,HEIGHT);
         f.setLayout(null);
         f.setVisible(true);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
