4b4546b1cec05336f72f6ac5b1862b1560423f86
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2022-11-11 12:50:39 -0500

    added comments for rulespage

--- src/main/java/gui/RulesPage.java
+++ src/main/java/gui/RulesPage.java
@@ -27,14 +27,18 @@ public class RulesPage {
 
         dialogueBox.f.getContentPane().setBackground(col);
 
+        // add title label for rules box
         rulesTitleLabel = new Label();
         rulesTitleLabel.createLabel(30,50, 40,WIDTH-100,200, dialogueBox.f, "Revised Rules of Scrabble", Color.BLACK);
         rulesTitleLabel.setCentreAlignment();
 
+        // add header label for rules box
         rulesHeaderLabel = new Label();
         rulesHeaderLabel.createLabel(16,50, 100, WIDTH-100,30, dialogueBox.f, "These rules were revised from the official Hasbro Rules of Scrabble", Color.BLACK);
         rulesHeaderLabel.setCentreAlignment();
 
+        // add rules to the rules page from the revised rules of scrabble
+        // TODO: go through and format this correctly or use another file that isn't in markdown
         File rules = new File("src/main/java/gui/resources/revised_rules_of_scrabble.md");
         Scanner reader = new Scanner(rules);
         String finalString = "";
@@ -45,6 +49,7 @@ public class RulesPage {
             finalString = finalString + data + "\n";
         }
 
+        // set rules text
         rulesTextLabel = new Label();
         rulesTextLabel.createMultiLineLabel(12,30, 200, WIDTH-100,500, dialogueBox.f, finalString, Color.BLACK);
     }
