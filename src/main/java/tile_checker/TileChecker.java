d4ec5bbef288f505ca20055b4e4211714cf1bef5
ClaireRamsumair <113072178+ClaireRamsumair@users.noreply.github.com>
2022-11-13 12:48:07 -0500

    Update TileChecker.java

--- src/main/java/tile_checker/TileChecker.java
+++ src/main/java/tile_checker/TileChecker.java
@@ -8,6 +8,18 @@ import java.util.List;
 
 public class TileChecker implements PlacementChecker {
     
+     public boolean validateMove(ArrayList<List<Integer>> move, GameBoard board) { //call to other functions that will validate move
+    if (!isConsecutive) { //if tiles aren't consecutive, return false
+        return false;
+    }
+    if (!isTouching) { //if tiles aren't touching already played tiles, return false
+        return false;
+    }
+    // TODO: insert call to wordchecker function after it is written
+    else { //if everything is valid, return true
+        return true;
+    }
+    
     @Override
     public boolean isValid(int row, int column, GameBoard board) {
         if (!Objects.equals(board.getBoardCellValue(row, column), "-")) { // checks that no letters are already in that space
