064c2fb9d27d423f97c7dd3e075b717571c86a54
Jazli <jazlim140@gmail.com>
2022-11-14 15:34:54 -0500

    Implemented PlayerManager, added setCell to BoardManager, getValue function to LetterBag, made ScoringSystem method static.

---
+++ src/main/java/games_manager/PlayerManager.java
@@ -0,0 +1,56 @@
+package games_manager;
+import java.util.Random;
+
+import entities.*;
+
+public class PlayerManager {
+
+    public void drawTile(Player player, LetterBag bag) {
+        /* Precondition: Player has less than 7 tiles in hand
+           Draws a tile to the players hand
+         */
+        Random rand = new Random();
+        Cell letter = new Cell();
+        String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        int int_random;
+        String character;
+
+        do {
+            int_random = rand.nextInt(26); // get a random number from 0-25
+            character = alphabet[int_random]; // use that number to get the associated letter
+        } while (bag.getValue(character) != 0); // check if that letter still has tiles in the bag
+
+        bag.removeTile(character); // remove that tile from the bag
+        BoardManager.boardManagerSetCellValue(letter, character); // update the cell to have the random value
+        int i = 0;
+        while (player.getHand()[i] != null) { // loop to find index of hand that is null
+            i++;
+        }
+        player.getHand()[i] = letter; // assign the null space the value of the new hand
+        ScoringSystem.initializeHandScore(player.getHand(), player);
+    }
+
+    public static void setName(Player player, String new_name) {
+        player.setName(new_name);
+    }
+
+    public static String getName(Player player) {
+        return player.getName();
+    }
+
+    public static void setScore(int new_score, Player player) {
+        player.setScore(new_score);
+    }
+
+    public static int getScore(Player player) { return player.getScore(); }
+
+    public static void setHand(Cell[] new_hand, Player player){ player.setHand(new_hand); }
+
+    public static Cell[] getHand(Player player) { return player.getHand(); }
+
+
+
+
+}
+
+
