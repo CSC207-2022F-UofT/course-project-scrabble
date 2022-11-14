6311ba2900dd7644c735b752f2904dfd33c618a6
Jazli <jazlim140@gmail.com>
2022-11-14 15:35:25 -0500

    This was Umair's changes

--- src/main/java/games_manager/ScoringSystem.java
+++ src/main/java/games_manager/ScoringSystem.java
@@ -5,23 +5,18 @@ import java.util.HashMap;
 
 
 public class ScoringSystem implements ScoreCalculator{
-    public final int[] LETTER_SCORES = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
-            1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-    public final String[] LETTERS = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
-            "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
-    public HashMap<String, Integer> letter_to_score = new HashMap<>();
-
-    public ScoringSystem() {
-        for(int i = 0; i<LETTERS.length; i++)
-            letter_to_score.put(LETTERS[i], LETTER_SCORES[i]);
-    }
-
-
-    public void initializeHandScore(Cell[] letters, Player player){
+    public static void initializeHandScore(Cell[] tiles, Player player){
         // assigns scores to the cells in players hands
         // reassigns players hands to the hand with scores
-        for (Cell letter: letters){
+        final int[] letter_scores = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
+                1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+        final String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+                "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+        final HashMap<String, Integer> letter_to_score = new HashMap<>();
+
+        for (Cell letter: tiles){
             String value = BoardManager.boardManagerGetCellValue(letter); // gets the letter represented by the cell
             int score = letter_to_score.get(value); // gets the score of the letter
             BoardManager.boardManagerSetCellScore(letter, score); // changes the cell's score to that of the letter it represents
