4e0ad75ea126dcb5a810f65b46eb683ce18ab0e5
Umair Hussain <1851Umair@gmail.com>
2022-11-18 17:16:00 -0500

    Split DrawTiles interface into 3 Created fill hand Created removeTile and corresponding tests

--- src/main/java/UsecaseInterfaces/DrawTiles.java
+++
@@ -1,9 +0,0 @@
-package UsecaseInterfaces;
-
-import entities.Game;
-
-public interface DrawTiles { // interface for the draw tile use case
-    public void drawTile(Game game);
-    public void swapHand(Game game);
-    public void drawHand(Game game);
-}
--- src/test/java/games_manager/PlayerManagerTest.java
+++ src/test/java/games_manager/PlayerManagerTest.java
@@ -89,4 +89,73 @@ public class PlayerManagerTest {
         Assertions.assertEquals(bag_size_before, bag_size_after); // bag should not change size when shuffled
     }
 
+    @Test
+    public void fillHandTest(){
+        String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        Game game = new Game();
+        PlayerManager p_manager = new PlayerManager();
+        Player p1 = new Player("Jeff");
+        game.addPlayer(p1);
+        Player p = game.getCurrentPlayer();
+        LetterBag bag = game.getLetterBag();
+        int bag_size_before = 0 ;
+        int bag_size_after = 0 ;
+        p_manager.drawHand(game);
+
+        PlayerManager.getHand(p)[0] = null;
+        PlayerManager.getHand(p)[4] = null;
+        PlayerManager.getHand(p)[6] = null;
+
+        for (int i=0; i<26; i++){
+            // getting the size of bag after swap
+            bag_size_before += bag.getNumTile(alphabet[i]);
+        }
+
+        p_manager.fillHand(game);
+
+        for (int i=0; i<7; i++){
+            // checking if players hand is not null
+            Assertions.assertNotNull(p.getHand()[i]);
+        }
+
+        for (int i=0; i<26; i++){
+            // getting the size of bag after swap
+            bag_size_after += bag.getNumTile(alphabet[i]);
+        }
+        Assertions.assertEquals(bag_size_before-3, bag_size_after); // bag should not change size when shuffled
+
+    }
+
+    @Test
+    public void removeTileTestOneOccurence(){
+        Game game = new Game();
+        PlayerManager p_manager = new PlayerManager();
+        Player p1 = new Player("Jeff");
+        game.addPlayer(p1);
+        PlayerManager.getHand(p1)[1] = new Cell ("C", 3, 1);
+        PlayerManager.getHand(p1)[4] = new Cell ("A", 1, 1);
+        PlayerManager.getHand(p1)[5] = new Cell ("R", 1, 1);
+
+        p_manager.removeTile(game, "A");
+        Cell[] hand = PlayerManager.getHand(p1);
+        Assertions.assertNull(hand[4]);
+    }
+
+    @Test
+    public void removeTileTestMultiOccurence(){
+        Game game = new Game();
+        PlayerManager p_manager = new PlayerManager();
+        Player p1 = new Player("Jeff");
+        game.addPlayer(p1);
+        PlayerManager.getHand(p1)[1] = new Cell ("A", 1, 1);
+        PlayerManager.getHand(p1)[4] = new Cell ("A", 1, 1);
+        PlayerManager.getHand(p1)[5] = new Cell ("A", 1, 1);
+
+        p_manager.removeTile(game, "A");
+        Cell[] hand = PlayerManager.getHand(p1);
+        Assertions.assertNull(hand[1]);
+        Assertions.assertEquals("A", hand[4].getValue());
+        Assertions.assertEquals("A", hand[5].getValue());
+    }
+
 }
