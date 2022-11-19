5e23b78e2d7a34c2822d504e630066bd451c3f27
Umair-H <u.hussain@mail.utoronto.ca>
2022-11-19 18:12:21 -0500

    Added JavaDocs for PlayerManager and ScoringSystem and their associated interfaces

--- src/main/java/UsecaseInterfaces/WordScoreCalculator.java
+++ src/main/java/UsecaseInterfaces/WordScoreCalculator.java
@@ -1,11 +1,11 @@
 package UsecaseInterfaces;
-
-import entities.Cell;
 import entities.Game;
-import entities.GameBoard;
-
 import java.util.List;
 
+/**
+ * This interface is responsible for calculating scores of words
+ * @author Umair
+ */
 public interface WordScoreCalculator { // interface for word score
 
     public int calculateWordScore(Game game, List<List<Integer>> word); // returns score of word
