7d31bc3aba3cb04adf1e9109c1377432641f12df
Jazli14 <95947726+Jazli14@users.noreply.github.com>
2022-11-19 15:23:01 -0500

    Updated JavaDoc for interface GameSave
    Updated interface with author

--- src/main/java/UsecaseInterfaces/GameSave.java
+++ src/main/java/UsecaseInterfaces/GameSave.java
@@ -3,6 +3,11 @@ package UsecaseInterfaces;
 import entities.Game;
 import java.io.File;
 
+/**
+* This interface is responsible for saving the Game instance to the created file in "src/main/java/data/data.ser".
+* @author Jazli
+*/
+
 public interface GameSave {
     final static File filename = new File("src/main/java/data/data.ser");
 
