24e81b4634e08fc748dead797d79349376565497
Jazli <jazlim140@gmail.com>
2022-11-20 14:05:04 -0500

    Edited GameSaverAndLoaderTest to include an additional assertion test to confirm last tile multiplier and an additional Player was added to the game.

--- src/test/java/data/GameSaverAndLoaderTest.java
+++ src/test/java/data/GameSaverAndLoaderTest.java
@@ -5,8 +5,6 @@ import games_manager.BoardManager;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import java.util.List;
-
 public class GameSaverAndLoaderTest {
 
     @Test
@@ -14,17 +12,19 @@ public class GameSaverAndLoaderTest {
         // creating objects to be saved
         GameSaverSystem game_saver = new GameSaverSystem();
         GameCreator gameCreator = new GameCreator();
-        String[] playerInputNames = new String[]{"Billy", "Mario"};
+        String[] playerInputNames = new String[]{"Billy", "Mario", "Harry"};
         Game game = gameCreator.createNewGame(playerInputNames);
 
 
         Player p1 = game.getPlayers().get(0);
         Player p2 = game.getPlayers().get(1);
+        Player p3 = game.getPlayers().get(2);
         GameBoard board = game.getGameBoard();
         LetterBag bag = game.getLetterBag();
 
         // editing some objects
         BoardManager.boardManagerSetBoardCell(0, 0, new Cell("V", 4, 2), board);
+        p1.setScore(2);
         p2.setScore(25);
         bag.putTile("A");
         bag.removeTile("C");
@@ -40,19 +40,22 @@ public class GameSaverAndLoaderTest {
         GameLoaderSystem game_loader = new GameLoaderSystem();
         Game game = game_loader.loadGame();
 
-        // loaded objects returned in the following order: GameBoard, Player1, Player2, LetterBag
+        // loaded objects returned with a GameBoard, Player1, Player2 and a LetterBag
         assert game != null;
         // assigning objects the loaded values
         GameBoard board = game.getGameBoard();
         Player p1 = game.getPlayers().get(0);
         Player p2 = game.getPlayers().get(1);
+        Player p3 = game.getPlayers().get(2);
         LetterBag bag = game.getLetterBag();
 
         // assertions checking if the updated data that was saved in the previous test
         // is within the new objects
         Assertions.assertEquals(3, board.getBoard()[0][0].getMultiplier());
-        Assertions.assertEquals(0, p1.getScore());
+        Assertions.assertEquals(3, board.getBoard()[14][14].getMultiplier());
+        Assertions.assertEquals(2, p1.getScore());
         Assertions.assertEquals(25, p2.getScore());
+        Assertions.assertEquals(0, p3.getScore());
         Assertions.assertEquals(10, bag.getNumTile("A"));
         Assertions.assertEquals(1, bag.getNumTile("C"));
 
