6bc4dca2cf67aa912d69209464d9157e1a7ec4e5
NearTruth <55720636+NearTruth@users.noreply.github.com>
2022-11-30 22:22:39 -0500

    updated javacoc for turnmanager

--- src/main/java/Usecases/usecase_implementations/TurnManager.java
+++ src/main/java/Usecases/usecase_implementations/TurnManager.java
@@ -10,7 +10,15 @@ import entities.Game;
  *
  * @author jingw
  */
+
+
 public class TurnManager implements IncrementTurnUsecase{
+    
+    
+    /**
+     * This method is responsible for updating the turn counter of a specific game
+     * @param game The game entity to be updated
+     */
     @Override
     public void incrementTurn(Game game) {
         game.incrementTurn();
