076e256269e6033a8046b6141da7f9a2a3e3c1aa
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2022-12-01 10:02:24 -0500

    moved ScrabbleGame code to main so that application can be fun from Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
+import gui.pages.StartupPage;
+
 /**
- *
- * @author jingw
+ * The Main is the application that runs the Scrabble Game.
+ * @author Victor Zheng
+ * @version 1.0
+ * @since 2022-11-19
  */
+
 public class Main {
-    
-    
-    public static void main(String[] args) {
-        
-        
-        
+    /**
+     * Generates the startup page. Aka the welcome page or the first page we see
+     */
+    static void generateStartUpPage() {
+        StartupPage startupPage = new StartupPage();
+        startupPage.createStartupPage();
     }
-    
+
+    public static void main(String[] args) {
+        generateStartUpPage();
+    }
 }
