53213a8921c1cf188914c2b3e50d02cfb19203ea
Umair-H <u.hussain@mail.utoronto.ca>
2022-12-02 18:45:03 -0500

    Added JavaDocs for Player and LetterBag. Added tests for LetterBag and Player.

--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -8,50 +8,58 @@ import java.io.*;
 
 
 /**
- *
+ * This class is responsible for keeping track of the Player attributes used in a game of Scrabble
  * @author Clark
  */
 
 public class Player implements Serializable {
     private int score;
-    private String name;
+    private final String name;
     private Cell[] hand;
-    
+
+    /**
+     * This method is the constructor for the player class which sets up a player to default values
+     * @param name the name of the Player
+     */
     public Player(String name) {
         this.score = 0;
         this.name = name;
         this.hand = new Cell[7];
     }
-    
-    public Player(String loaded_name, int loaded_score, Cell[] loaded_hand) {
-        this.name = loaded_name;
-        this.score = loaded_score;
-        this.hand = loaded_hand;
-    }
-    
-    
-    
-    public void setName(String new_name) {
-        this.name = new_name;
-    }
-    
+
     public String getName() {
         return this.name;
     }
-    
+
+    /**
+     * This method sets the score of the player to new_score
+     * @param new_score The score player's score will be changed to
+     */
     public void setScore(int new_score) {
         this.score = new_score;
     }
-    
+
+    /**
+     * This method returns the player's score
+     * @return player's score
+     */
     public int getScore() {
         return this.score;
     }
-    
+
+    /**
+     * This method sets player's hand to new_hand where new_hand is of size 7
+     * @param new_hand the array of cells the player's hand will be changed to. Must be of size 7
+     */
     public void setHand(Cell[] new_hand) {
         this.hand = new_hand;
-        
+
     }
-    
+
+    /**
+     * This method is returns the player's hand
+     * @return player's hand
+     */
     public Cell[] getHand() {
         return this.hand;
     }
