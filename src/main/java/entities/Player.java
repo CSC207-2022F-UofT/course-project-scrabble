36380a82d3bf48b42c4961de5508c32e00e9865e
Umair Hussain <1851Umair@gmail.com>
2022-12-02 14:57:38 -0500

    Added java docs for Player Class
    Didn't add for methods that I thought may be removed due to them not being used.

--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -16,7 +16,11 @@ public class Player implements Serializable {
     private int score;
     private String name;
     private Cell[] hand;
-    
+
+    /**
+     * This method is the constructor for the player class which sets up a player to default values
+     * @param name the name of the Player
+     */
     public Player(String name) {
         this.score = 0;
         this.name = name;
@@ -28,9 +32,9 @@ public class Player implements Serializable {
         this.score = loaded_score;
         this.hand = loaded_hand;
     }
-    
-    
-    
+
+
+
     public void setName(String new_name) {
         this.name = new_name;
     }
@@ -38,20 +42,36 @@ public class Player implements Serializable {
     public String getName() {
         return this.name;
     }
-    
+
+    /**
+     * This method sets the score of the player to new_score
+     * @param new_score The score player's score will be changed to
+     */
     public void setScore(int new_score) {
         this.score = new_score;
     }
-    
+
+    /**
+     * This method returns the player's score
+     * @return player's score
+     */
     public int getScore() {
         return this.score;
     }
-    
+
+    /**
+     * This method sets player's hand to new_hand where new_hand is of size 7
+     * @param new_hand the array of cells the player's hand will be changed to. Must be of size 7
+     */
     public void setHand(Cell[] new_hand) {
         this.hand = new_hand;
         
     }
-    
+
+    /**
+     * This method is returns the player's hand
+     * @return player's hand
+     */
     public Cell[] getHand() {
         return this.hand;
     }
