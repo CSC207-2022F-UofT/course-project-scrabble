c159f9c4859a251cad99b6b09c7b21b7c5779a3f
Jazli <jazlim140@gmail.com>
2022-12-02 16:39:03 -0500

    Changed the "filename" file back to a String

--- src/main/java/usecases/usecase_interfaces/GameSaveUsecase.java
+++ src/main/java/usecases/usecase_interfaces/GameSaveUsecase.java
@@ -1,15 +1,14 @@
-package UsecaseInterfaces;
+package usecases.usecase_interfaces;
 
 import entities.Game;
-import java.io.File;
 
 /**
 * This interface is responsible for saving the Game instance to the created file in "src/main/java/data/data.ser".
 * @author Jazli
 */
 
-public interface GameSave {
-    File filename = new File("src/main/java/data/data.ser");
+public interface GameSaveUsecase {
+    String filename = "src/main/java/data/data.ser";
 
     void saveGame(Game game);
 
